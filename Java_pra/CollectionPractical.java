a6a6bc97f4b3be7d8a8b551c92c682093659030c
MatheeshaAnusari <anusarimatheesha@gmail.com>
2024-10-19 12:27:41 +0530

    Add files via upload

---
+++ Java_pra/CollectionPractical.java
@@ -0,0 +1,26 @@
+import java.util.Collection;
+import java.util.LinkedList;
+
+class CollectionPractical1{
+	public CollectionPractical1(){
+		Collection <Integer> c = new LinkedList();
+		c.add(5);
+		c.add(6);
+		c.add(4);
+		c.add(1);
+        c.add(2);
+		
+		System.out.println("is it empty"+c.isEmpty());
+		System.out.println("size="+c.size());
+		System.out.println("Is it contain element 5="+c.contains(5));
+		
+		c.remove(6);
+		
+		System.out.println("Remaining ="+c);
+		
+}
+    public static void main(String [] args)
+	{
+		new CollectionPractical1();
+	}		
+}
