f1eb72ea00aecb1634392e952bd3af1308e5c4d4
MatheeshaAnusari <anusarimatheesha@gmail.com>
2024-12-05 20:29:45 +0530

    Add files via upload

---
+++ Java_pra/FindFactorial.java
@@ -0,0 +1,18 @@
+
+public class FindFactorial{
+      public static long factorial (int n){
+	     if (n == 1)
+		      return 1;
+	     else
+              return n * factorial(n-1);
+		}
+       public static void main(String[]args){
+           //int result = factorial(5);
+           //System.out.println("Factorial is " + result);
+		   int mynum = 127 + 1;
+		   System.out.println("My number is " + mynum);
+   }
+}   
+	      
+		  
+    
