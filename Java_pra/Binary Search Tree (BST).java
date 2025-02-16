505fc5c873268e11c054b759298ded0f0c5c0225
MatheeshaAnusari <anusarimatheesha@gmail.com>
2025-02-16 20:49:54 +0530

    Add files via upload

---
+++ Java_pra/Binary Search Tree (BST).java
@@ -0,0 +1,51 @@
+class BST {
+    class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    private Node root;
+
+    public void insert(int data) {
+        root = insertRec(root, data);
+    }
+
+    private Node insertRec(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+        if (data < root.data) {
+            root.left = insertRec(root.left, data);
+        } else {
+            root.right = insertRec(root.right, data);
+        }
+        return root;
+    }
+
+    public void inorder() {
+        inorderRec(root);
+        System.out.println();
+    }
+
+    private void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.data + " ");
+            inorderRec(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BST tree = new BST();
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(70);
+        tree.inorder();
+    }
+}
